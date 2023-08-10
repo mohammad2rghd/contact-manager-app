c73ae8d156f314ec40e67686bae61d4ec45e1407
mohammadg <mohammad2rghdd@gmail.com>
2023-08-10 20:20:18 +0330

    basename in index.js changed

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import "react-toastify/dist/ReactToastify.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="contact-manager-app">
+    <BrowserRouter basename="contact-manager-app.iran.liara.run">
       <App />
     </BrowserRouter>
   </React.StrictMode>
