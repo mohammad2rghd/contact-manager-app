722be24b47e0ad137972dc129c2e0aae05fb0b89
mohammadg <mohammad2rghdd@gmail.com>
2023-08-17 01:49:24 +0330

    services updated to work in auth token

--- src/services/contactService.js
+++ src/services/contactService.js
@@ -4,14 +4,14 @@ const SERVER_URL = "https://contact-manager-app-api.iran.liara.run";
 const authToken = "YOUR_AUTH_TOKEN";
 
 // @desc Get All Contacts
-// @route GET http://localhost:9000/contacts
+// @route GET https://contact-manager-app-api.iran.liara.run/contacts
 export const getAllContacts = () => {
   const url = `${SERVER_URL}/contacts`;
   return axios.get(url);
 };
 
 // @desc Get Contact With Contact ID
-// @route GET http://localhost:9000/contacts/:contactId
+// @route GET https://contact-manager-app-api.iran.liara.run/contacts/:contactId
 export const getContact = (contactId) => {
   const url = `${SERVER_URL}/contacts/${contactId}`;
   return axios.get(url, {
@@ -22,7 +22,7 @@ export const getContact = (contactId) => {
 };
 
 // @desc  Get All Groups
-// @route GET http://localhost:9000/groups
+// @route GET https://contact-manager-app-api.iran.liara.run/groups
 export const getAllGroups = () => {
   const url = `${SERVER_URL}/groups`;
   return axios.get(url, {
@@ -33,7 +33,7 @@ export const getAllGroups = () => {
 };
 
 // @desc  Get Group Name With Group ID
-// @route GET http://localhost:9000/groups/:groupId
+// @route GET https://contact-manager-app-api.iran.liara.run/groups/:groupId
 export const getGroup = (groupId) => {
   const url = `${SERVER_URL}/groups/${groupId}`;
   return axios.get(url, {
@@ -44,7 +44,7 @@ export const getGroup = (groupId) => {
 };
 
 // @desc  Create New Contact
-// @route POST http://localhost:9000/contacts
+// @route POST https://contact-manager-app-api.iran.liara.run/contacts
 export const createContact = (contact) => {
   const url = `${SERVER_URL}/contacts`;
   return axios.post(url, contact, {
@@ -55,7 +55,7 @@ export const createContact = (contact) => {
 };
 
 // @desc  Update Contact
-// @route PUT http://localhost:9000/contacts/:contactId
+// @route PUT https://contact-manager-app-api.iran.liara.run/contacts/:contactId
 export const updateContact = (contact, contactId) => {
   const url = `${SERVER_URL}/contacts/${contactId}`;
   return axios.put(url, contact, {
@@ -66,7 +66,7 @@ export const updateContact = (contact, contactId) => {
 };
 
 // @desc  Delete Contact
-// @route DELETE http://localhost:9000/contacts/:contactId
+// @route DELETE https://contact-manager-app-api.iran.liara.run/contacts/:contactId
 export const deleteContact = (contactId) => {
   const url = `${SERVER_URL}/contacts/${contactId}`;
   return axios.delete(url, {
